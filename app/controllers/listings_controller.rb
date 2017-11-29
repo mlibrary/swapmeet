4a3e677022ffc3415aa0fa4c42d0f3b14564d9e5
Noah Botimer <botimer@umich.edu>
2017-11-29 11:38:19 -0500

    Revert to typical Rails approaches for examples
    This commit makes a few changes to take the master branch to a more
    typical Rails approach, with models directly inheriting from
    ApplicationRecord and traditional controller/view implementation. The
    idea is that the policy examples will be more approachable if they work
    with a more traditional layout.
    
    There are also many things to figure out with the repository and use
    case approaches, so it is partially expedient to start traditional and
    partially about setting up a refactoring/evolution exercise.
    
    This also adds Slim for views and an .envrc for direnv use.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,23 +1,24 @@
 class ListingsController < ApplicationController
   def index
-    render locals: { listings: newspaper.listings }
+    render locals: { listings: Listing.all }
   end
 
   def new
-    render locals: { listing: newspaper.new_listing }
+    render locals: { listing: Listing.new }
   end
 
   def create
-    l = newspaper.new_listing
-    listing_params.tap do |p|
-      l.title = p[:title]
-      l.body = p[:body]
+    listing = Listing.new(listing_params)
+    if listing.save
+      redirect_to listings_path
+    else
+      render :new, locals: { listing: listing }
     end
-    newspaper.add_listing(l)
-    redirect_to listings_path
   end
 
+  private
+
   def listing_params
-    params.permit(:title, :body)
+    params.require(:listing).permit(:title, :body)
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,25 +1,2 @@
-class Listing
-  attr_accessor :id, :newspaper, :title, :body
-
-  def initialize(id: nil, title: '', body: '')
-    @id = id
-    @title = title
-    @body = body
-  end
-
-  def publish
-    newspaper.add_listing(self)
-  end
-
-  def to_h
-    {
-        id: id,
-        title: title,
-        body: body
-    }
-  end
-
-  def ==(other)
-    to_h == other.to_h
-  end
+class Listing < ApplicationRecord
 end
