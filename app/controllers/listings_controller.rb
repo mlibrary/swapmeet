aa53ce77e6b6eb4a6c41de02627138264dd62fbe
Noah Botimer <botimer@umich.edu>
2017-11-30 13:58:47 -0500

    AEIM-723 - Allow updating a listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,8 +3,18 @@ class ListingsController < ApplicationController
     render locals: { listings: Listing.all }
   end
 
+  def show
+    listing = find_listing
+    render locals: { listing: listing }
+  end
+
   def new
     render locals: { listing: Listing.new }
+  end
+
+  def edit
+    listing = find_listing
+    render locals: { listing: listing }
   end
 
   def create
@@ -16,7 +26,20 @@ class ListingsController < ApplicationController
     end
   end
 
+  def update
+    listing = find_listing
+    if listing.update(listing_params)
+      redirect_to listing
+    else
+      render :edit, locals: { listing: listing }
+    end
+  end
+
   private
+
+  def find_listing
+    Listing.find(params[:id])
+  end
 
   def listing_params
     params.require(:listing).permit(:title, :body)
