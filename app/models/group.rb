f72396c417dc7fd4402727af27e25107a77f2db3
Greg Kostin <gkostin@umich.edu>
2017-12-07 15:46:24 -0500

    create join table users groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Group < ApplicationRecord
+  has_many :children, class_name: "Group", foreign_key: "parent_id"
+  belongs_to :parent, class_name: "Group", optional: true
+  has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_and_belongs_to_many :groups
+
   def self.nobody
     new(username: '<nobody>', display_name: '(No one)', email: '').tap(&:readonly!)
   end
