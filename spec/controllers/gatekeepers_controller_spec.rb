1132d4f341a3155b76fc7248a1ba5dabee00f4ea
Greg Kostin <gkostin@umich.edu>
2017-12-08 11:47:57 -0500

    spec support controllers helper shared examples

--- spec/controllers/gatekeepers_controller_spec.rb
+++ spec/controllers/gatekeepers_controller_spec.rb
@@ -3,104 +3,65 @@
 require 'rails_helper'
 
 RSpec.describe GatekeepersController, type: :controller do
-  let(:user) { build(:user, id: 1) }
-  let(:target) { build(:gatekeeper, id: 1) }
+  context 'policy' do
+    context 'unauthorized' do
+      describe '#create' do
+        it_should_behave_like 'unauthorized#create', :gatekeeper, :Gatekeeper
+      end
 
-  before do
-    allow(controller).to receive(:current_user).and_return(current_user)
-    allow(user).to receive(:persisted?).and_return(true)
-    allow(Gatekeeper).to receive(:find).with('1').and_return(target)
-  end
+      describe '#destory' do
+        it_should_behave_like 'unauthorized#destroy', :gatekeeper, :Gatekeeper
+      end
 
-  context 'anonymous user' do
-    let(:current_user) { User.guest }
+      describe '#edit' do
+        it_should_behave_like 'unauthorized#edit', :gatekeeper, :Gatekeeper
+      end
 
-    describe '#create' do
-      subject { post :create, params: { gatekeeper: { name: 'Name', display_name: 'Display Name' } } }
-      before { subject }
-      it { expect(response).to be_unauthorized }
+      describe '#index' do
+        it_should_behave_like 'unauthorized#index', :gatekeeper
+      end
+
+      describe '#new' do
+        it_should_behave_like 'unauthorized#new', :gatekeeper
+      end
+
+      describe '#show' do
+        it_should_behave_like 'unauthorized#show', :gatekeeper, :Gatekeeper
+      end
+
+      describe '#update' do
+        it_should_behave_like 'unauthorized#update', :gatekeeper, :Gatekeeper
+      end
     end
 
-    describe '#destory' do
-      subject { delete :destroy, params: { id: target.id } }
-      before { subject }
-      it { expect(response).to be_unauthorized }
-    end
+    context 'authorized' do
+      describe '#create' do
+        it_should_behave_like 'authorized#create', :gatekeeper, :Gatekeeper
+      end
 
-    describe '#edit' do
-      subject { get :edit, params: { id: target.id } }
-      before { subject }
-      it { expect(response).to be_unauthorized }
-    end
+      describe '#destory' do
+        it_should_behave_like 'authorized#destroy', :gatekeeper, :Gatekeeper
+      end
 
-    describe '#index' do
-      subject { get :index }
-      before { subject }
-      it { expect(response).to be_unauthorized }
-    end
+      describe '#edit' do
+        it_should_behave_like 'authorized#edit', :gatekeeper, :Gatekeeper
+      end
 
-    describe '#new' do
-      subject { get :new }
-      before { subject }
-      it { expect(response).to be_unauthorized }
-    end
+      describe '#index' do
+        it_should_behave_like 'authorized#index', :gatekeeper
+      end
 
-    describe '#show' do
-      subject { get :show, params: { id: target.id } }
-      before { subject }
-      it { expect(response).to be_unauthorized }
-    end
+      describe '#new' do
+        it_should_behave_like 'authorized#new', :gatekeeper
+      end
 
-    describe '#update' do
-      subject { post :update, params: { id: target.id, gatekeeper: { name: 'Name', display_name: 'Display Name' } } }
-      before { subject }
-      it { expect(response).to be_unauthorized }
-    end
-  end
+      describe '#show' do
+        it_should_behave_like 'authorized#show', :gatekeeper, :Gatekeeper
+      end
 
-  context 'authenticated user' do
-    let(:current_user) { user }
-
-    describe '#create' do
-      subject { post :create, params: { gatekeeper: { name: 'Name', display_name: 'Display Name' } } }
-      before { subject }
-      it { expect(response).to redirect_to gatekeeper_path(Gatekeeper.last) }
-    end
-
-    describe '#destory' do
-      subject { delete :destroy, params: { id: target.id } }
-      before { subject }
-      it { expect(response).to redirect_to gatekeepers_path }
-    end
-
-    describe '#edit' do
-      subject { get :edit, params: { id: target.id } }
-      before { subject }
-      it { expect(response).to be_success }
-    end
-
-    describe '#index' do
-      subject { get :index }
-      before { subject }
-      it { expect(response).to be_success }
-    end
-
-    describe '#new' do
-      subject { get :new }
-      before { subject }
-      it { expect(response).to be_success }
-    end
-
-    describe '#show' do
-      subject { get :show, params: { id: target.id } }
-      before { subject }
-      it { expect(response).to be_success }
-    end
-
-    describe '#update' do
-      subject { post :update, params: { id: target.id, gatekeeper: { role: 'Role' } } }
-      before { subject }
-      it { expect(response).to redirect_to gatekeeper_path(target) }
+      describe '#update' do
+        it_should_behave_like 'authorized#update', :gatekeeper, :Gatekeeper
+      end
     end
   end
 end
