41e29855f0ccf88d81db10c13330ce37629a78ee
Greg Kostin <gkostin@umich.edu>
2017-12-09 14:07:37 -0500

    application policy spec

--- app/policies/users_policy.rb
+++ app/policies/users_policy.rb
@@ -1,24 +1,6 @@
 # frozen_string_literal: true
 
-# Sample resource-oriented, multi-rule policy
-class UsersPolicy
-  attr_reader :subject, :object
-
-  def initialize(subject, object)
-    @subject = subject
-    @object = object
-  end
-
-  def create?
-    return true if subject.root?
-    false
-  end
-
-  def destroy?
-    return true if subject.root?
-    false
-  end
-
+class UsersPolicy < ApplicationPolicy
   def edit?
     return true if subject.root?
     subject.known? && subject.client == object.client
@@ -29,11 +11,6 @@ class UsersPolicy
     subject.known?
   end
 
-  def new?
-    return true if subject.root?
-    false
-  end
-
   def show?
     return true if subject.root?
     subject.known?
@@ -42,9 +19,5 @@ class UsersPolicy
   def update?
     return true if subject.root?
     subject.known? && subject.client == object.client
-  end
-
-  def authorize!(action, message = nil)
-    raise NotAuthorizedError.new(message) unless send(action)
   end
 end
