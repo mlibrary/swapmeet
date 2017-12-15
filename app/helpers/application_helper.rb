c05e48a6a2fa1a6b1931142645942efc90e2e865
Noah Botimer <botimer@umich.edu>
2017-12-15 18:22:11 -0500

    CP-11 - Fix up user display name links or text
    Add a helper for the logic of outputting just the user's display name
    when a guest/nobody, and linking to the user when known. It should
    probably be done with a presenter, but this is the simplest thing for
    now.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def user_display_name_link(user)
+    if user.known?
+      link_to user.display_name, user
+    else
+      user.display_name
+    end
+  end
 end
