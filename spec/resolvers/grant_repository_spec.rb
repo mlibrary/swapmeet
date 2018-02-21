414e05f68c5800c41911481d98eff10569bfd0b6
Noah Botimer <botimer@umich.edu>
2018-02-21 14:42:18 -0500

    Move checkpoint files out

--- spec/resolvers/grant_repository_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'grant_repository'
-
-RSpec.describe GrantRepository do
-  it "does not have any grants" do
-    expect(subject.grants_for(nil, nil, nil)).to eq([])
-  end
-end
