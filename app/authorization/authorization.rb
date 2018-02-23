5a819634e08a6d164448e0fa36fc964d5dd50261
Greg Kostin <gkostin@umich.edu>
2018-02-23 14:11:40 -0500

    permit?

--- app/authorization/authorization.rb
+++ app/authorization/authorization.rb
@@ -1,4 +1,29 @@
 # frozen_string_literal: true
 
 class Authorization
+  attr_reader :user, :action, :target
+
+  def initialize(user, action, target)
+    @user = user
+    @action = action
+    @target = target
+  end
+
+  def permit?
+    permit_user_action? && permit_user_target? && permit_action_target?
+  end
+
+  private
+
+    def permit_user_action?
+      false
+    end
+
+    def permit_action_target?
+      false
+    end
+
+    def permit_user_target?
+      false
+    end
 end
--- spec/authorization/authorization_spec.rb
+++ spec/authorization/authorization_spec.rb
@@ -5,7 +5,18 @@ require 'rails_helper'
 RSpec.describe Authorization, type: :authorization do
   subject { authorization }
 
-  let(:authorization) { described_class.new }
+  let(:authorization) { described_class.new(user, action, target) }
+  let(:user) { double('user') }
+  let(:action) { double('action') }
+  let(:target) { double('target') }
 
-  it { is_expected.not_to be_nil }
+  describe '#permit?' do
+    subject { authorization.permit? }
+
+    it { is_expected.to be false }
+
+    context 'permit' do
+      xit { is_expected.to be true }
+    end
+  end
 end
